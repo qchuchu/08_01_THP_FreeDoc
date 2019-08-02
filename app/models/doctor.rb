0ffa5181cc15ef08fad5ec5d170c91d8f0d07f58
qchuchu <quentin.churet@outlook.fr>
2019-08-02 12:29:04 +0200

    Final Commit

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
-  has_and_belongs_to_many :specialty
+  has_and_belongs_to_many :specialties
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,8 @@ DatabaseCleaner.clean_with(:truncation)
 
 100.times do
   first_name, last_name = Faker::FunnyName.two_word_name.split(' ')
-  specialty = ['Allergy & Immunology','Anesthesiology', 'Dermatology', 'Diagnostic Radiology', 'Emergency Medicine', 'Family Medicine', 'Internal Medicine', 'Medical Genetics', 'Neurology', 'Nuclear Medicine', 'Obstetrics & Gynecology', 'Ophtalmology', 'Pathology', 'Pediatrics', 'Physical Medicine & Rehabilitation', 'Preventive Medicine', 'Psychiatry', 'Radiation Oncology', 'Surgery', 'Urology'].sample
-  zip_code = ''
-  5.times{|_| zip_code = zip_code + rand(0..9).to_s}
-  Doctor.create(first_name: first_name, last_name: last_name, specialty: specialty, zip_code: zip_code)
+  city = Faker::Address
+  Doctor.create(first_name: first_name, last_name: last_name, zip_code: city.zip_code, city: city.city)
 end
 
 100.times do
@@ -27,12 +25,20 @@ end
   Patient.create(first_name: first_name, last_name: last_name)
 end
 
-10000.times do
+100.times do
   date = Time.at((rand+1)*Time.now.to_i)
   doctor = Doctor.all.sample
   patient = Patient.all.sample
   Appointment.create(patient: patient, doctor: doctor, date: date)
 end
 
-Doctor.all.each do |doctor|
-  doctor.update(city: )
+specialties =  ['Allergy & Immunology','Anesthesiology', 'Dermatology', 'Diagnostic Radiology', 'Emergency Medicine', 'Family Medicine', 'Internal Medicine', 'Medical Genetics', 'Neurology', 'Nuclear Medicine', 'Obstetrics & Gynecology', 'Ophtalmology', 'Pathology', 'Pediatrics', 'Physical Medicine & Rehabilitation', 'Preventive Medicine', 'Psychiatry', 'Radiation Oncology', 'Surgery', 'Urology']
+
+specialties.each do |spe|
+  Specialty.create(name: spe)
+end
+
+100.times do
+  doctor = Doctor.all.sample
+  doctor.specialties << Specialty.all.sample
+end
